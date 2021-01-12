8860157bcc01e5dff74abcaa294f55a8c8682ecb
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-12 12:49:06 +0530

    XAMARIN-38639 - Sample and screenshot added

---
+++ ScheduleXamarin/ScheduleXamarin/ScheduleXamarin/Behavior/SchedulerPageBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Syncfusion.SfSchedule.XForms;
+using Xamarin.Forms;
+
+namespace ScheduleXamarin
+{
+    public class SchedulerPageBehavior : Behavior<ContentPage>
+    {
+        SfSchedule schedule;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            this.schedule = bindable.Content.FindByName<SfSchedule>("Schedule");
+            this.schedule.MoveToDate = DateTime.Now.Date.AddHours(11);
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+        }
+    }
+}
